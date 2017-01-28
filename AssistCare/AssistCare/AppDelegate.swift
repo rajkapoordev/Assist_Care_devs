d2563d21ef9d49051d94653065fee656fd80e15c
Sonal <lanetteam.sonalmaniya@gmail.com>
2017-01-28 10:33:48 +0530

    Manage xcode project file.

--- AssistCare/AssistCare/AppDelegate.swift
+++ AssistCare/AssistCare/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.white]
 
        
-        let vc = searchCareGiverOnMapVC(nibName: "searchCareGiverOnMapVC", bundle: nil)
+        let vc = CreateProfileFirst(nibName: "CreateProfileFirst", bundle: nil)
 
         
 //       let vc = CategoryVC(nibName: "CategoryVC", bundle: nil)
