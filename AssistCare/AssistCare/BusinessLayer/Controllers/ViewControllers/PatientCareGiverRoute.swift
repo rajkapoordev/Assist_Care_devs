fcd1d5687f19751a3db6efd848dd775fa7136a32
madhusmart <developer91@iTilaks-Mac.local>
2017-02-03 18:58:38 +0530

    Facebook Integration ,make changes in NewAppoinmentVC,PatientCareGiverRoute

--- AssistCare/AssistCare/BusinessLayer/Controllers/ViewControllers/PatientCareGiverRoute.swift
+++ AssistCare/AssistCare/BusinessLayer/Controllers/ViewControllers/PatientCareGiverRoute.swift
@@ -8,25 +8,43 @@
 
 import UIKit
 import MapKit
-class PatientCareGiverRoute: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+class PatientCareGiverRoute: UIViewController,UITableViewDelegate,UITableViewDataSource, UITextViewDelegate {
     @IBOutlet var scrollView: UIScrollView!
 
+    
+    @IBOutlet var txtVwInstruction: UITextView!
+    @IBOutlet var btnSkip: UIButton!
+    @IBOutlet var lbWhatWentWrong: UILabel!
+    @IBOutlet var lbTimeWentWrong: UILabel!
+    @IBOutlet var lbNameWentWrong: UILabel!
+    @IBOutlet var imgProfileWentWrong: UIImageView!
+    @IBOutlet var vwPopupWentWrong: UIView!
     @IBOutlet var tblView: UITableView!
     @IBOutlet var vWTop: UIView!
     @IBOutlet var mapView: MKMapView!
+    
+    @IBAction func btnSkip(_ sender: Any) {
+        
+    }
+    
+    let border = UIView()
     var header = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtVwInstruction.delegate = self
+        vwPopupWentWrong.layer.cornerRadius = 5.0
         tblView.register(UINib(nibName:"MedicationPrompt",bundle : nil), forCellReuseIdentifier: "MedicationPrompt")
         scrollView.contentSize = CGSize(width: 0, height: (vWTop.frame.height + tblView.frame.height))
-        
-        
-        
 
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidLayoutSubviews() {
+        vwPopupWentWrong.frame = CGRect(x: 20, y: (screenSize.height/2) - (self.vwPopupWentWrong.bounds.size.height / 2), width: screenSize.width - 40, height: self.vwPopupWentWrong.bounds.size.height)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -62,6 +80,32 @@ class PatientCareGiverRoute: UIViewController,UITableViewDelegate,UITableViewDat
         return cell
     }
     
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.gray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+        
+        border.frame = CGRect(x: txtVwInstruction.frame.origin.x, y: txtVwInstruction.frame.origin.y+txtVwInstruction.frame.height-2, width: textView.frame.width, height: 2)
+        border.backgroundColor = appUIColorFromRGB(rgbValue: GREEN_COLOR, alpha: 1.0)
+        txtVwInstruction.superview!.insertSubview(border, aboveSubview: textView)
+        
+    }
+    
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if txtVwInstruction.subviews.contains(border){
+            border.removeFromSuperview()
+        }
+        let height = heightForView(text: txtVwInstruction.text, font: txtVwInstruction.font!, width: txtVwInstruction.frame.size.width)
+        txtVwInstruction.frame = CGRect(x: txtVwInstruction.frame.origin.x, y: txtVwInstruction.frame.origin.y, width: txtVwInstruction.frame.size.width, height: height+10)
+        
+        border.frame = CGRect(x: txtVwInstruction.frame.origin.x, y: txtVwInstruction.frame.origin.y+txtVwInstruction.frame.height-2, width: textView.frame.width, height: 2)
+        border.backgroundColor = appUIColorFromRGB(rgbValue: GREEN_COLOR, alpha: 1.0)
+        txtVwInstruction.superview!.insertSubview(border, aboveSubview: textView)
+    }
+
 
     /*
     // MARK: - Navigation
